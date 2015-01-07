00a1dfb0831661418deb5028ded0bc85712c298d
Aviad Blumenfeld <aviadblumenfeld@Aviads-MacBook-Pro.local>
2015-01-07 21:31:54 +0200

    sec

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# socket-io-chat
-Socket.io chat example
+# Socket Demonstration App
+
+A express node.js app built with Socket.io. For demonstration purposes and a tutorial.
+
+## Example
+
+The example show you a simple implementation of chat app using sockets
+
+### What to do?
+
+Start chatting
+
+## App run
+
+Click on the 'Play' button at the top toolbar.
+
+![Express-angular](http://i.imgur.com/BjXZgWs.png)
--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
+// load socket server module
 var io = require('socket.io')(http);
-var port  	 = process.env.PORT || 9090;
+// set default server port
+var port = process.env.PORT || 9090;
 
-
+// set client index url (view)
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+// handle chat messages
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         console.log('message: ' + msg);
@@ -15,6 +18,7 @@ io.on('connection', function(socket){
     });
 });
 
+// start the server
 http.listen(port, function(){
     console.log('Express server listening on %d', port);
 });
